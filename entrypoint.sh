f6f12d2b21e97e109d1a9a7e43db2f060d40ab94
Siamak Sadeghianfar <ssadeghi@redhat.com>
2018-07-24 19:22:14 +0200

    added maven mirror configuration

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,33 +15,21 @@ export GROUP_ID=$(id -g)
 if ! grep -Fq "${USER_ID}" /etc/passwd; then
     envsubst < ${HOME}/passwd.template > /etc/passwd
     envsubst < ${HOME}/group.template > /etc/group
-
-    # The trick below is the one that is suggested here
-    # https://docs.openshift.com/enterprise/3.2/creating_images/guidelines.html#openshift-enterprise-specific-guidelines
-    # But every new exec call won't work because 
-    # LD_PRELOAD, NSS_WRAPPER_PASSWD and NSS_WRAPPER_GROUP
-    # are not set there.
-    #
-    # envsubst < ${HOME}/passwd.template > /tmp/passwd
-    # envsubst < ${HOME}/group.template > /tmp/group
-    # export LD_PRELOAD=/usr/local/lib/libnss_wrapper.so
-    # export NSS_WRAPPER_PASSWD=/tmp/passwd
-    # export NSS_WRAPPER_GROUP=/tmp/group
     touch ${HOME}/is_arbitrary_user
 fi
 
-# SSH agent is currently not supported but if we
-# do want to support it we can uncomment the code below
-# and fix SSHAgentLauncher that has SSHD port hardcoded
-# to 22 and will expect the service to be available at that
-# port. And we cannot use port 22 because it's a 
-# privileged port and OpenShift will complain.
-#
-# SSHD_PORT=2222
-# /usr/bin/ssh-keygen -A
-# mkdir -p /home/user/.ssh
-# chmod 700 /home/user/.ssh
-# echo "UsePrivilegeSeparation no" > /home/user/.ssh/sshd_config
-# /usr/sbin/sshd -D -p ${SSHD_PORT} -f /home/user/.ssh/sshd_config &
+# check for the MAVEN_MIRROR_URL env variable, if its available then set maven mirrors in $HOME/.m2/settings.xml
+configure_mirrors() {
+  if [ -n "$MAVEN_MIRROR_URL" ]; then
+    xml="    <mirror>\
+      <id>mirror.default</id>\
+      <url>$MAVEN_MIRROR_URL</url>\
+      <mirrorOf>external:*</mirrorOf>\
+    </mirror>"
+    sed -i "s|<!-- ### configured mirrors ### -->|$xml|" "${HOME}/.m2/settings.xml"
+  fi
+}
+
+configure_mirrors
 
 exec "$@"
